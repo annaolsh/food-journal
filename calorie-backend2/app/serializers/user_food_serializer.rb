a7f950ec521714ea3a2b4248fd60aad21f768058
hellosylvee <helloimsylvia@gmail.com>
2017-05-10 17:27:04 -0400

    Created persisting info on page

--- calorie-backend2/app/serializers/user_food_serializer.rb
+++ calorie-backend2/app/serializers/user_food_serializer.rb
@@ -1,7 +1,7 @@
 class UserFoodSerializer < ActiveModel::Serializer
   # belongs_to :user
   # belongs_to :food
-  attributes :id, :calories, :food_name, :food_id, :user_name, :user_id
+  attributes :id, :calories, :date, :food_name, :food_id, :user_name, :user_id
   def food_name
     object.food.name
   end
--- calorie-client/index.js
+++ calorie-client/index.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
   $('#submitButton').on('click', function(e){
     e.preventDefault();
-    const user_id = $('#user_id').val()
+    const user_id = $('#name-input').val()
     const date = $('#date').val()
-    const food_id = $('#food_id').val()
+    const food_id = $('#food-input').val()
     const calories = $('#calories').val()
-    //const url = 'http://localhost:3000/api/v1/journals'
 
     $.ajax({
       method: 'POST',
@@ -18,26 +17,28 @@ $(document).ready(function() {
       }
     }).then(function(data) {
       console.log(data)
-      $('#showResults').append(`<li id=${data.food_id} class="food-item">${data.food_id} with ${data.calories} calories</li>`)
+      $('#showResults').append(`<li>${data.food_name} (${data.calories} cal.) </li>`)
     })
   })
 
-  // $('body').on('click', '.food-item', function(){
-  //   alert($(this).attributes("id"))
-  // })
-})
+  $.ajax({
+    method: 'GET',
+    url: 'http://localhost:3000/api/v1/user_foods',
+    success: function(data){
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth()+1; //January is 0!
+      var yyyy = today.getFullYear();
+      var present_day = `${yyyy}-0${mm}-${dd}`
+      var newArray = [];
 
-// $.ajax({
-//       method: 'POST',
-//       url: 'http://localhost:3000/api/v1/cities',
-//       data: {
-//         state_name: stateName,
-//         city_name: cityName
-//       } //:state_name is from params, stateName is the input value
-//      }).then(function(data) {
-//       //  console.log(data)
-//        $('#success').append(`
-//         <div>${data.name}</div>
-//          `)
-//
-//     }); //line 22
+      data.forEach(function(food) {
+        if (food.date === present_day) {
+          newArray.push(`<li> ${food.food_name} (${food.calories} cal.) </li>`)
+        }
+      })
+      $('#todayDate').append(present_day)
+      $('#showResults').append(newArray)
+    }
+  })
+})
