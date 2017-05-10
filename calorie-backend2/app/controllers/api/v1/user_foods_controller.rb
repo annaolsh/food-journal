dbf614b85bb974450952399b2ae8718c027c0e6e
hellosylvee <helloimsylvia@gmail.com>
2017-05-10 14:36:14 -0400

    updated controller

--- calorie-backend2/app/controllers/api/v1/user_foods_controller.rb
+++ calorie-backend2/app/controllers/api/v1/user_foods_controller.rb
@@ -1,34 +1,8 @@
 class Api::V1::UserFoodsController < ApplicationController
   def index
     @userfoods = UserFood.all
-    # render json: @userfoods
-    # render json: {userfoods: @userfoods, name: @userfoods.map{ |userfood| userfood.user.name}}
     render json: @userfoods
-      ##exclude: [user: [:id]]
-      #fields: { user: [:name], food: [:name]}
-
-    
-
-    # render json: @userfoods.to_json(
-    #   only: [:id, :calories],
-    #   include: [
-    #     user: { only: [:name]},
-    #     food: { only: [:name]}
-    # ])
   end
-
-  render json: @post, include: [:media, author: [:website]],
-    fields: { media: [:title], author: { website: [:url] } }
-
-  # def show
-  #     @post = Post.find(params[:id])
-  #     respond_to do |format|
-  #       format.html { render :show }
-  #       format.json { render json: @post.to_json(only: [:title, :description, :id], include: [author: { only: [:name]}]) }
-  #     end
-  #   end
-
-
 
 
   def create
@@ -38,7 +12,6 @@ class Api::V1::UserFoodsController < ApplicationController
       calories: params[:calories],
       date: params[:date]
     )
-
     if @userfood.valid?
       @userfood.save
       render json: @userfood
