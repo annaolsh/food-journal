a35c703d8c0b818a58ec2bdf4d1e296a927e9f45
hellosylvee <helloimsylvia@gmail.com>
2017-05-11 12:07:25 -0400

    updated object return

--- calorie-client/js/index.js
+++ calorie-client/js/index.js
@@ -16,123 +16,41 @@ $(document).ready(function() {
         date: date
       }
     }).then(function(data) {
-      console.log(data)
-      $('#showResults').html(`<li>${data.food_name} (${data.calories} cal.) </li>`)
-            var today = new Date();
-            var dd = today.getDate();
-            var mm = today.getMonth()+1; //January is 0!
-            var yyyy = today.getFullYear();
-            var present_day = `${yyyy}-0${mm}-${dd}`
-            var sum = 0;
+      $.ajax({
+        method: 'GET',
+        url: 'http://localhost:3000/api/v1/user_foods',
+        success: function(dataResponse) {
+          console.log("Hello there?")
+          console.log(dataResponse) //that one big giant hash
+          console.log(data) //that one single lonely object
+          var todayDate = new Date().toJSON().slice(0, 10);
+          var current_user = data.user_id
 
-            //Prototype Clean, removes all the undefined values
-            Array.prototype.clean = function(deleteValue) {
-              for (var i = 0; i < this.length; i++) {
-                if (this[i] == deleteValue) {
-                  this.splice(i, 1);
-                  i--;
-                }
-              }
-              return this;
-            };
+          // for(var i = 0; i < dataResponse.length; i++) {
+          //   if (dataResponse[i].user_id === current_user && dataResponse[i].date === todayDate){
+          //     current_user_food.push(dataResponse[i])
+          //   }
+          // }
 
-            //We want calories based on specific user and today's date
-            var array = data.map(function(obj){
-              if(obj.user_name === "Daniel" && present_day === obj.date) {
-              return obj.calories //this includes an array with calories AND undefined
-              }
-            })
-
-            //mutated array with only calories
-            array.clean(undefined)
-
-            //returns total calories of specific user and today's date
-            var SUM = array.reduce(function(sum, val){
-              return sum + val
-            }, 0)
-            $('#totalCalories').html(SUM)
+          var current_user_food = dataResponse.filter(function(user_food){
+            return user_food.user_id === current_user && user_food.date === todayDate
           })
-    })
+          console.log(current_user_food)
+          // var current_user_food = {}
+          // var todayDate = new Date().toJSON().slice(0, 10);
+          // for(var i = 0; i < dataResponse.length; i++){
+          //   if (dataResponse[i].user_id === current_user && dataResponse[i].date === todayDate){
+          //     current_user_food[dataResponse[i].food_name] = dataResponse[i].calories
+          //   }
+          // }
+          // var result = ''
+          // for (var key in current_user_food) {
+          //   result += `<li>${key + " " + current_user_food[key] + " cal."}</li>`
+          // }
+          // $('#showResults').html(result)
 
-    //displays all foods for today
-  //   $.ajax({
-  //     method: 'GET',
-  //     url: 'http://localhost:3000/api/v1/user_foods',
-  //     success: function(data){
-  //       // debugger
-  //       var today = new Date();
-  //       var dd = today.getDate();
-  //       var mm = today.getMonth()+1; //January is 0!
-  //       var yyyy = today.getFullYear();
-  //       var present_day = `${yyyy}-0${mm}-${dd}`
-  //       var sum = 0;
-  //
-  //       //Prototype Clean, removes all the undefined values
-  //       Array.prototype.clean = function(deleteValue) {
-  //         for (var i = 0; i < this.length; i++) {
-  //           if (this[i] == deleteValue) {
-  //             this.splice(i, 1);
-  //             i--;
-  //           }
-  //         }
-  //         return this;
-  //       };
-  //
-  //       //We want calories based on specific user and today's date
-  //       var array = data.map(function(obj){
-  //         if(obj.user_name === "Daniel" && present_day === obj.date) {
-  //         return obj.calories //this includes an array with calories AND undefined
-  //         }
-  //       })
-  //
-  //       //mutated array with only calories
-  //       array.clean(undefined)
-  //
-  //       //returns total calories of specific user and today's date
-  //       var SUM = array.reduce(function(sum, val){
-  //         return sum + val
-  //       }, 0)
-  //       $('#totalCalories').html(SUM)
-  //     }
-  //   })
-  // })
-
-  $.ajax({
-    method: 'GET',
-    url: 'http://localhost:3000/api/v1/user_foods',
-    success: function(data){
-      var today = new Date();
-      var dd = today.getDate();
-      var mm = today.getMonth()+1; //January is 0!
-      var yyyy = today.getFullYear();
-      var present_day = `${yyyy}-0${mm}-${dd}`
-      var newArray = [];
-
-      console.log(data)
-
-      data.forEach(function(food) {
-        if (food.date === present_day) {
-          newArray.push(`<li> ${food.food_name} (${food.calories} cal.) </li>`)
         }
       })
-      var sum = data.reduce(function(sum, ele) {
-        return sum + ele.calories;
-      }, 0);
-
-      $('#todayDate').append(present_day)
-      $('#totalCalories').append(sum)
-      $('#showResults').append(newArray)
-    }
+    })
   })
-
-  const CHART = document.getElementById("lineChart");
-  console.log(CHART);
-
-  var today = new Date();
-  var dd = today.getDate();
-  var mm = today.getMonth()+1; //January is 0!
-  var yyyy = today.getFullYear();
-  var present_day = `${yyyy}-0${mm}-${dd}`
-  var sum = 0;
-
 })
